a71f5107bed271fa8dcc9a4ad56139d4c0e0dfd4
Nelson <nelsonenriquesierraperez@gmail.com>
2024-06-06 17:55:43 -0500

    ProyectoFinal

---
+++ src/main/java/com/Biblioteca/app/BibliotecaProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.Biblioteca.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaProyectoApplication.class, args);
+	}
+
+}
